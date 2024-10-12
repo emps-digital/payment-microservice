cac46c44088214a494acb2c872b598ed44d75cd6
Bonner <neilsonlima@gmail.com>
2024-10-12 19:34:08 -0300

    Initial project setup

---
+++ src/test/kotlin/br/com/pm/api/PaymentMicroserviceApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.pm.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PaymentMicroserviceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
